1d6e38821175e71dce2095d1e475e36caddcc52a
Tahi_ri96@itstep.edu.az <Tahi_ri96@itstep.edu.az>
2021-07-07 13:26:57 +0400

    i add new icons and many backround color

--- OnStore-In-Wpf/MainWindow.xaml.cs
+++ OnStore-In-Wpf/MainWindow.xaml.cs
@@ -19,49 +19,63 @@ namespace OnStore_In_Wpf
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
-        public List<Product> Products = new List<Product>
-        {
-            new Product{
-             Name="Bread",
-              Price=0.50,
-               ImagePath="Images/bread1.png",
-               Description=""
-            },
-            new Product{
-             Name="Pizza",
-              Price=14,
-               ImagePath="Images/pizza.png",
-            },
-            new Product{
-             Name="Burger",
-              Price=4,
-               ImagePath="Images/burger.jpg",
-               Description=""
-            },
-            new Product{
-             Name="Cola",
-              Price=1.50,
-               ImagePath="Images/cola.png",
-            },
-            new Product{
-             Name="Cips",
-              Price=2,
-               ImagePath="Images/cips.png",
-               Description=""
-            },
-            new Product{
-             Name="Juice",
-              Price=2.50,
-               ImagePath="Images/juice.png",
-            }
-        };
+        public List<Product> products;
+        public List<Product> Products { get;set; }
+
         public MainWindow()
         {
-            InitializeComponent();
+            products = new List<Product>() {
+                new Product
+                {
+                    Name = "Bread",
+                    Price = 0.50,
+                    ImagePath = "Images/bread1.png",
+                    Description = "sdzrahszrgh"
+                },
+            new Product
+            {
+                Name = "Pizza",
+                Price = 14,
+                ImagePath = "Images/pizza.png",
+            },
+            new Product
+            {
+                Name = "Burger",
+                Price = 4,
+                ImagePath = "Images/burger.png",
+                Description = ""
+            },
+            new Product
+            {
+                Name = "Cola",
+                Price = 1.50,
+                ImagePath = "Images/cola.png",
+            },
+            new Product
+            {
+                Name = "Cips",
+                Price = 2,
+                ImagePath = "Images/cips.png",
+                Description = ""
+            },
+            new Product
+            {
+                Name = "Juice",
+                Price = 2.50,
+                ImagePath = "Images/juice.png",
+            }
+            };
             DataContext = this;
-            mainListbox.ItemsSource = Products;
+            Products = products;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mainListbox.SelectedItem = editListbox.Items;
         }
     }
 }
